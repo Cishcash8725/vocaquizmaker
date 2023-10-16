7f9a77e7e4cf668834daf359cd84bc6882f5c361
cishcash8725 <58230494+Cishcash8725@users.noreply.github.com>
2023-10-16 18:55:12 +0900

    Add files via upload

---
+++ vocaquizmaker/src/main/java/cishcash8725/vocaquizmaker/VocaquizmakerApplication.java
@@ -0,0 +1,13 @@
+package cishcash8725.vocaquizmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaquizmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaquizmakerApplication.class, args);
+	}
+
+}
---
+++ vocaquizmaker/src/test/java/cishcash8725/vocaquizmaker/VocaquizmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package cishcash8725.vocaquizmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaquizmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
